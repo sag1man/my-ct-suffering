d439962feaf67627fa96d866923bdaff9e758957
sag1man <sasha@sagiman.ru>
2025-09-29 00:19:03 +0300

    add auto web rendering and deploying

---
+++ javascripts/katex.js
@@ -0,0 +1,13 @@
+document$.subscribe(() => {
+  if (typeof renderMathInElement === "function") {
+    renderMathInElement(document.body, {
+      delimiters: [
+        {left: "$$", right: "$$", display: true},
+        {left: "$",  right: "$",  display: false},
+        {left: "\\(", right: "\\)", display: false},
+        {left: "\\[", right: "\\]", display: true}
+      ],
+      throwOnError: false
+    });
+  }
+});
